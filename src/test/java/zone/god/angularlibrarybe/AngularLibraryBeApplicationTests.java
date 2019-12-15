f417ae7bc4031526b8bb4d48d36543cf45c33012
zonesama <trannhatthanh31@gmail.com>
2019-12-15 20:34:04 +0700

    First Commit.

---
+++ src/test/java/zone/god/angularlibrarybe/AngularLibraryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package zone.god.angularlibrarybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularLibraryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
